0dc409827316914956b8b2b4665bcbc9e54e1ac7
Holler János <hollerjanika@gmail.com>
2026-05-31 13:59:38 +0200

    Replace <a> elements to <Link> component

--- eccomerce-project/src/pages/OrdersPage.jsx
+++ eccomerce-project/src/pages/OrdersPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import { Header } from '../components/Header'
 import './OrdersPage.css';
 
@@ -11,12 +12,12 @@ export function OrdersPage() {
 
       <div className="header">
         <div className="left-section">
-          <a href="/" className="header-link">
+          <Link to="/" className="header-link">
             <img className="logo"
               src="images/logo-white.png" />
             <img className="mobile-logo"
               src="images/mobile-logo-white.png" />
-          </a>
+          </Link>
         </div>
 
         <div className="middle-section">
@@ -28,16 +29,16 @@ export function OrdersPage() {
         </div>
 
         <div className="right-section">
-          <a className="orders-link header-link" href="/orders">
+          <Link className="orders-link header-link" to="/orders">
 
             <span className="orders-text">Orders</span>
-          </a>
+          </Link>
 
-          <a className="cart-link header-link" href="/checkout">
+          <Link className="cart-link header-link" to="/checkout">
             <img className="cart-icon" src="images/icons/cart-icon.png" />
             <div className="cart-quantity">3</div>
             <div className="cart-text">Cart</div>
-          </a>
+          </Link>
         </div>
       </div>
 
@@ -87,11 +88,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
 
               <div className="product-image-container">
@@ -115,11 +116,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -166,11 +167,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
