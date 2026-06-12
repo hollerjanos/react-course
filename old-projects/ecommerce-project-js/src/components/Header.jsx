80f68e90665639f8c3fbd221dd66fea36a270398
Holler János <hollerjanika@gmail.com>
2026-06-12 11:27:36 +0200

    Convert ecommerce project to TypeScript

---
+++ old-projects/ecommerce-project-js/src/components/Header.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
+import LogoWhite from '../assets/images/logo-white.png';
+import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
+import CartIcon from '../assets/images/icons/cart-icon.png';
+import SearchIcon from '../assets/images/icons/search-icon.png';
+import './Header.css';
+
+export function Header({ cart }) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const searchText = searchParams.get("search");
+
+  const [search, setSearch] = useState(searchText || '');
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  }
+
+  let TotalQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    TotalQuantity += cartItem.quantity;
+  });
+
+  return (
+    <div className="header">
+      <div className="left-section">
+        <NavLink to="/" className="header-link">
+          <img className="logo"
+            src={LogoWhite} />
+          <img className="mobile-logo"
+            src={MobileLogoWhite} />
+        </NavLink>
+      </div>
+
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search"
+          data-testid="header-search-bar"
+          value={search} onChange={updateSearchInput}/>
+
+        <button className="search-button"
+          data-testid="header-search-button"
+          onClick={searchProducts}>
+          <img className="search-icon" src={SearchIcon}/>
+        </button>
+      </div>
+
+      <div className="right-section">
+        <NavLink className="orders-link header-link" to="/orders"
+          data-testid="header-orders-link">
+
+          <span className="orders-text">Orders</span>
+        </NavLink>
+
+        <NavLink className="cart-link header-link" to="/checkout"
+          data-testid="header-cart-link">
+          <img className="cart-icon" src={CartIcon}/>
+          <div className="cart-quantity">{TotalQuantity}</div>
+          <div className="cart-text">Cart</div>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
---
+++ old-projects/ecommerce-project-js/src/pages/checkout/CartItemDetails.jsx
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { formatMoney } from '../../utils/money';
+import "./CartItemDetails.css";
+
+export function CartItemDetails({ cartItem, loadCart }) {
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
+  const deleteCartItem = async () => {
+    await axios.delete(`/api/cart-items/${cartItem.productId}`);
+    await loadCart();
+  }
+
+  const updateQuantity = async () => {
+    if (isUpdatingQuantity) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(quantity)
+      });
+      await loadCart();
+      setIsUpdatingQuantity(false);
+    } else {
+      setIsUpdatingQuantity(true);
+    }
+    setIsUpdatingQuantity(!isUpdatingQuantity);
+  }
+
+  const updateQuantityInput = (event) => {
+    setQuantity(event.target.value);
+  }
+
+  const handleQuantityKeyDown = (event) => {
+    const keyPressed = event.key;
+
+    if (keyPressed === "Enter") {
+      updateQuantity();
+    } else if (keyPressed === "Escape") {
+      setQuantity(cartItem.quantity);
+      setIsUpdatingQuantity(false);
+    }
+  }
+
+  return (
+    <>
+      <img className="product-image"
+        src={cartItem.product.image}
+        data-testid="cart-item-image"/>
+
+      <div className="cart-item-details">
+        <div className="product-name"
+          data-testid="cart-item-name">
+          {cartItem.product.name}
+        </div>
+        <div className="product-price"
+          data-testid="cart-item-price">
+          {formatMoney(cartItem.product.priceCents)}
+        </div>
+        <div className="product-quantity">
+          <span data-testid="cart-item-quantity">
+            Quantity: {isUpdatingQuantity
+              ? <input type="text" className="quantity-textbox"
+                  value={quantity} onChange={updateQuantityInput}
+                  onKeyDown={handleQuantityKeyDown}
+                />
+              : <span className="quantity-label">{cartItem.quantity}</span>
+            }
+          </span>
+          <span className="update-quantity-link link-primary"
+            onClick={updateQuantity}>
+            Update
+          </span>
+          <span className="delete-quantity-link link-primary"
+            data-testid="cart-item-delete-quantity-link"
+            onClick={deleteCartItem}>
+            Delete
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
